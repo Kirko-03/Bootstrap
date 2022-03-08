4fbba3cfba1994210b2ac0855d92b53eb863fc5a
Kirito <kurulluvarov@gmail.com>
2022-03-08 18:44:00 +0300

    add modal window,cards

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,62 @@
 import React from "react";
 import {Slider} from "./Slider";
+import {Button, Card, Col, Container, Row} from "react-bootstrap";
 
 
 export const Home = () => {
     return<>
        <Slider/>
+        <Container style={{paddingTop:'2rem'}}>
+            <Row>
+                <Col>
+                    <Card style={{width:'18rem'}}>
+                        <Card.Img variant={'top'} src={'https://ocean.ru/images/news/2020/ioi-conf-1.jpg'}/>
+                        <Card.Body>
+                            <Card.Title>
+                                Programm
+                            </Card.Title>
+                            <Card.Text>
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis, velit?
+                            </Card.Text>
+                            <Button variant='primary'>
+                                Learn more
+                            </Button>
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col>
+                    <Card style={{width:'18rem'}}>
+                        <Card.Img variant={'top'} src={'https://ocean.ru/images/news/2020/ioi-conf-1.jpg'}/>
+                        <Card.Body>
+                            <Card.Title>
+                                Programm
+                            </Card.Title>
+                            <Card.Text>
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis, velit?
+                            </Card.Text>
+                            <Button variant='primary'>
+                                Learn more
+                            </Button>
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col>
+                    <Card style={{width:'18rem'}}>
+                        <Card.Img variant={'top'} src={'https://ocean.ru/images/news/2020/ioi-conf-1.jpg'}/>
+                        <Card.Body>
+                            <Card.Title>
+                                Programm
+                            </Card.Title>
+                            <Card.Text>
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis, velit?
+                            </Card.Text>
+                            <Button variant='primary'>
+                                Learn more
+                            </Button>
+                        </Card.Body>
+                    </Card>
+                </Col>
+            </Row>
+        </Container>
     </>
 }
--- src/components/Navibar.jsx
+++ src/components/Navibar.jsx
@@ -1,6 +1,6 @@
-import {Button, Container, Nav, Navbar} from "react-bootstrap";
+import {Button, Container, Form, Modal, Nav, Navbar} from "react-bootstrap";
 import {NavLink} from "react-router-dom";
-import React from "react";
+import React, {useState} from "react";
 import styled from "styled-components";
 
 const Styles = styled.div `
@@ -13,29 +13,61 @@ a, .navbar-brand, .navbar-nav .nav-link{
 `
 
 export const Navibar = () => {
-    return <Styles>
-        <Navbar collapseOnSelect expand='lg' bg="dark" variant='dark' >
-        <Container>
-            <Navbar.Brand className='ml-2'>Kirik studies programming</Navbar.Brand>
-            <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
-            <Navbar.Collapse id="responsive-navbar-nav">
-                <Nav className='me-auto'>
-                    <Nav.Item className='me-2'>
-                        <NavLink to='/'>Home</NavLink>
-                    </Nav.Item>
-                    <Nav.Item className='me-2'>
-                        <NavLink to='/users'>Users</NavLink>
-                    </Nav.Item>
-                    <Nav.Item className='me-2'>
-                        <NavLink to='/about'>About</NavLink>
-                    </Nav.Item>
-                </Nav>
-                <Nav>
-                    <Button variant="primary" className='me-2'>LogIn</Button>
-                    <Button  variant="primary">LogOut</Button>
-                </Nav>
-            </Navbar.Collapse>
-        </Container>
-        </Navbar>
+    const [show,setShow] = useState(false)
+    const handleClose = () => setShow(false)
+    const handleOpen = () => setShow(true)
+
+    return <>
+        <Styles>
+            <Navbar collapseOnSelect expand='lg' bg="dark" variant='dark' >
+                <Container>
+                    <Navbar.Brand className='ml-2'>Kirik studies programming</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className='me-auto'>
+                            <Nav.Item className='me-2'>
+                                <NavLink to='/'>Home</NavLink>
+                            </Nav.Item>
+                            <Nav.Item className='me-2'>
+                                <NavLink to='/users'>Users</NavLink>
+                            </Nav.Item>
+                            <Nav.Item className='me-2'>
+                                <NavLink to='/about'>About</NavLink>
+                            </Nav.Item>
+                        </Nav>
+                        <Nav>
+                            <Button variant="primary" onClick={()=>handleOpen()} className='me-2'>LogIn</Button>
+                            <Button  variant="primary">Sign Out</Button>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
         </Styles>
+    <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+            <Modal.Title>Login</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+            <Form>
+                <Form.Group controllId="fromBasicEmail">
+                    <Form.Label>
+                        Email Adress
+                    </Form.Label>
+                    <Form.Control type='email' placeholder="Enter email "/>
+                    <Form.Text className='text-muted'>We'll never share your email</Form.Text>
+                </Form.Group>
+                <Form.Group controllId="fromBasicPassword">
+                    <Form.Label>
+                        Password
+                    </Form.Label>
+                    <Form.Control type='password' placeholder="Enter password "/>
+                    <Form.Text className='text-muted'>We'll never share your password</Form.Text>
+                </Form.Group>
+                <Form.Group controllId="fromBasicCheckbox">
+                    <Form.Check type='checkbox' label='Remember me'/>
+                   </Form.Group>
+            </Form>
+        </Modal.Body>
+    </Modal>
+    </>
 }
